364eab955b1f95aad45df662b3a3477336264888
KhasV <khasanovarvara@gmail.com>
2020-09-06 06:08:49 +0300

    add separator

--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -6,16 +6,13 @@ import './style.scss';
 import { firebaseService } from '../../services';
 import { groupByDate, convertToIsoDate } from '../../utils/date';
 import { Task } from './Task';
-
+import { Separator } from './Separator';
 
 export const Tasks = () => {
     const period = new URLSearchParams(useLocation().search).get('period');
     const [isCreateTaskOpen, setIsCreateTaskOpen] = useState(false);
     const [tasks, setTasks] = useState(null);
  
-
- 
-
     useEffect(() => {
         const date = new Date();
         let startDate, endDate;
@@ -34,28 +31,20 @@ export const Tasks = () => {
         return () => unsubscribe();
     }, [period]);
 
-
     return (
         <>
             <Header />
             <Container flex>
- 
                 <section className="tasks">
-
-
                     {Object.entries(groupByDate(tasks)).map(([date, groupedTasks], index) => {
                         const mappedTasks = groupedTasks.map((task, i) => 
                         <Task
                             key={task.id}
                             {...task}
                         />);
-                        return  <div className="separator">
-                                    <div className="title">{index}</div><hr />
-                                    <div className="list">{date}</div>
-                                </div>;
+                        return  <Separator key={index} title={date}>{mappedTasks}</Separator>
                     }
                     )}
-
                 </section>
             </Container>
         </>
